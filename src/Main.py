70c337cba9e0999c0538fab244dc1ce9e54d49d4
EcoFercho <erwin0pisis@gmail.com>
2025-06-25 04:03:50 -0400

    fix cors

--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from flask_cors import CORS 
 from Config.Config import Config
 from Infra.Models.EstudianteModels import db 
 from Infra.Routes.EstudianteRoutes import estudiante  
@@ -6,39 +7,26 @@ from Infra.Routes.LoginRoutes import login
 from Infra.Routes.RegisterLoteRoute import registrarLote
 from Infra.Routes.PaisCiudadRoutes import paisCiudad
 from werkzeug.exceptions import HTTPException
-from flask_cors import CORS, cross_origin 
 
 app = Flask(__name__)
 
-CORS(app, resources={
-    r"/api/*": {
-        "origins": [
-            "https://front-loginv1.vercel.app",
-            "http://localhost:3000"
-        ],
-        "supports_credentials": True,
-        "allow_headers": [
-            "Content-Type",
-            "Authorization",
-            "X-Requested-With"
-        ],
-        "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-    }
-})
-
+# Configura CORS
+CORS(app) 
 app.config.from_object(Config)
 db.init_app(app)
-# Registramos el Blueprint que contiene las rutas de estudiantes
+
+# Registramos los Blueprints
 app.register_blueprint(estudiante, url_prefix="/api/estudiantes")
 app.register_blueprint(login, url_prefix="/api/login")
 app.register_blueprint(registrarLote, url_prefix="/api/registrarLoteEstudiantes")
 app.register_blueprint(paisCiudad, url_prefix="/api/")
 
-# Ruta principal o por defecto
+# Ruta principal
 @app.route('/')
 def index():
     return "Hola esta es la API de BackendGenSof - microservicio/Estudiantes."
 
+# Manejo de errores
 @app.errorhandler(404)
 def not_found_error(error):
     return jsonify({
@@ -46,14 +34,12 @@ def not_found_error(error):
         "message": "La ruta solicitada no existe. Verifica la URL y el método."
     }), 404
 
-
 @app.errorhandler(HTTPException)
 def handle_http_exception(e):
     return jsonify({
         "status": e.code,
         "message": e.description
     }), e.code
-
 
 if __name__ == '__main__':
     app.run(debug=True)
