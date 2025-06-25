d55887fe830e1bf520e88cecf0b262334f631174
EcoFercho <erwin0pisis@gmail.com>
2025-06-25 03:03:53 -0400

    Cors implementacion fix 4

--- src/Infra/Routes/LoginRoutes.py
+++ src/Infra/Routes/LoginRoutes.py
@@ -1,11 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from Infra.Controllers import LoginController
 
 # Crear un Blueprint para agrupar rutas del recurso estudiante
 login = Blueprint('login', __name__)
 
-@login.route("/", methods=["POST"])
+@login.route("/", methods=["POST", "OPTIONS"])
 def loginEstudiante():
+    if request.method == "OPTIONS":
+        return "", 200 
     return LoginController.loginEstudiante()
 
 @login.route("/cambiarContrasenia", methods=["PUT"])
