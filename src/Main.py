9ce18aee096f5afdf228ae28c069bebbb6f61034
EcoFercho <erwin0pisis@gmail.com>
2025-06-25 05:17:07 -0400

    fix cors 6

--- src/Main.py
+++ src/Main.py
@@ -1,62 +1,6 @@
-from flask import Flask, jsonify, Blueprint
-from flask_cors import CORS
-from flask_talisman import Talisman
-from Config.Config import Config
-from Infra.Models.EstudianteModels import db
-from Infra.Routes.EstudianteRoutes import estudiante
-from Infra.Routes.LoginRoutes import login
-from Infra.Routes.RegisterLoteRoute import registrarLote
-from Infra.Routes.PaisCiudadRoutes import paisCiudad
-from werkzeug.exceptions import HTTPException
+from Infra.Routes.app import crearApp
 
-def crearApp():
-    app = Flask(__name__)
-    
-    # Seguridad HTTPS (opcional pero recomendable en producción)
-    Talisman(app, content_security_policy=None)
-
-    # Permitir CORS para TODAS las rutas, TODOS los orígenes
-    CORS(app, supports_credentials=True)
-
-    app.config.from_object(Config)
-    db.init_app(app)
-
-    api_blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
-
-    @api_blueprint.route('/')
-    def home():
-        return jsonify({
-            'data': f"{app.config.get('APP_NAME', 'BackendGenSof')} - Microservicio Estudiantes",
-            'message': 'API en funcionamiento',
-            'status': 200
-        })
-
-    # Registro de rutas hijas
-    api_blueprint.register_blueprint(estudiante, url_prefix="/estudiantes")
-    api_blueprint.register_blueprint(login, url_prefix="/login")
-    api_blueprint.register_blueprint(registrarLote, url_prefix="/registrarLoteEstudiantes")
-    api_blueprint.register_blueprint(paisCiudad, url_prefix="/")
-
-    app.register_blueprint(api_blueprint)
-
-    @app.errorhandler(404)
-    def not_found_error(error):
-        return jsonify({
-            "status": 404,
-            "message": "La ruta solicitada no existe. Verifica la URL y el método.",
-            "error": str(error)
-        }), 404
-
-    @app.errorhandler(HTTPException)
-    def handle_http_exception(e):
-        return jsonify({
-            "status": e.code,
-            "message": e.description,
-            "error": str(e)
-        }), e.code
-
-    return app
+applicacion = crearApp()
 
 if __name__ == '__main__':
-    app = crearApp()
-    app.run(debug=True)
+    applicacion.run()
